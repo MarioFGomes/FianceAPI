efffc15ac68893bedb4066451a6bbfb7e7c1abd7
Mário Gomes <mario.gomes@bravantic.co.ao>
2025-07-04 16:55:57 +0100

    refactor:project repository

--- src/Bckend/Finance.Domain/Repositories/IBaseRepository.cs
+++ src/Bckend/Finance.Domain/Repositories/IBaseRepository.cs
@@ -8,8 +8,10 @@ using System.Threading.Tasks;
 namespace Finance.Domain.Repositories;
 public interface IBaseRepository<TEntity> where TEntity : class {
     Task AddOneAsync(TEntity entity);
+    Task AddManyAsync(List<TEntity> entity);
     Task DeleteAsync(Expression<Func<TEntity, bool>> filterExpression);
     Task ReplaceOneAsync(Expression<Func<TEntity, bool>> filterExpression, TEntity entity);
+    Task ReplaceManyAsync(List<TEntity> entities, Func<TEntity, object> keySelector);
     Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> filterExpression);
     Task<IEnumerable<TEntity>> GetAllAsync(int page = 1, int pageSize = 10);
     Task<IEnumerable<TEntity>> GetAllAsync();
@@ -17,4 +19,7 @@ public interface IBaseRepository<TEntity> where TEntity : class {
     Task<int> CountAsync(Expression<Func<TEntity, bool>> filterExpression);
     IQueryable<TEntity> GetQueryable();
     Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filterExpression);
+    void BeginTransaction();
+    void CommitTransaction();
+    void RollbackTransaction();
 }
--- src/Bckend/Finance.Infrastructure/DataAcess/Repositories/UserRepository.cs
+++ src/Bckend/Finance.Infrastructure/DataAcess/Repositories/UserRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Finance.Domain.Entities;
 using Finance.Domain.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Finance.Infrastructure.DataAcess.Repositories; 
 public class UserRepository: BaseRepository<User>, IUserRepository {
-    public UserRepository(FinanceContext _context): base(_context) { }
+    public UserRepository(FinanceContext _context, ILogger<BaseRepository<User>> logger) : base(_context,logger) { }
   
 
 }
