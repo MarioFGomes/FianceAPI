2aa4e20656b31d0c63da1873d810aa81fe48b64e
Mário Gomes <marioferreiragomes333@gmail.com>
2025-07-06 12:36:25 +0100

    refactor:improving user search route

--- src/Bckend/Finance.API/Controllers/UserController.cs
+++ src/Bckend/Finance.API/Controllers/UserController.cs
@@ -1,12 +1,13 @@
-﻿using Finance.Application.UseCase.User.FetchUser;
+﻿using Finance.API.Filter;
+using Finance.Application.UseCase.User.FetchUser;
 using Finance.Communication.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Finance.API.Controllers; 
 public class UserController: FinanceController {
 
-    [HttpGet]
-    [Route("fetch/search")]
+    [HttpGet("fetch")]
+    [ServiceFilter(typeof(AuthenticatedUser))]
     [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> Fetch([FromServices] IFecthUserUseCase useCase, [FromQuery] string search) {
         var resultado = await useCase.Execute(search);
